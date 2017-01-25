449cc91bbd08002735859b76a9a6f9289715407c
Carlos E. Chan Mtz <carlosechanm@MacBook-Pro-de-Carlos.local>
2017-01-24 20:04:14 -0500

    Initial Commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('myproperties')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
